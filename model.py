febc3256c12227bab05f34c0d10c83d2558c15ec
FHanko <FlorianHanko@hotmail.com>
2024-06-03 23:20:30 +0200

    Add swapping constraint

--- model.py
+++ model.py
@@ -35,15 +35,23 @@ def init_model(state: State):
             model.add_modulo_equality(x[t, a], pos[t, a], state.width)
             model.add_division_equality(y[t, a], pos[t, a], state.height)
 
-    dx = {}
-    dy = {}
     for t in range(state.time - 1):
         for a in range(state.agents):
-            dx[t, a] = model.new_int_var(0, state.width, f"dx_{t}_{a}")
-            dy[t, a] = model.new_int_var(0, state.height, f"dy_{t}_{a}")
-            model.add_abs_equality(dx[t, a], x[t, a] - x[t + 1, a])
-            model.add_abs_equality(dy[t, a], y[t, a] - y[t + 1, a])
-            model.add(dx[t, a] + dy[t, a] <= 1)
+            dx = model.new_int_var(0, state.width, f"dx_{t}_{a}")
+            dy = model.new_int_var(0, state.height, f"dy_{t}_{a}")
+            model.add_abs_equality(dx, x[t, a] - x[t + 1, a])
+            model.add_abs_equality(dy, y[t, a] - y[t + 1, a])
+            model.add(dx + dy <= 1)
+
+    # No swapping of agents.
+    for t in range(state.time - 1):
+        for a in range(state.agents):
+            for a2 in range(state.agents):
+                atoa2 = model.new_bool_var("")
+                a2toa = model.new_bool_var("")
+                model.add(pos[t, a] == pos[t + 1, a2]).only_enforce_if(atoa2)
+                model.add(pos[t + 1, a] == pos[t, a2]).only_enforce_if(a2toa)
+                model.add_at_most_one(atoa2, a2toa)
 
     # Set the objective.
     at_goal = {}
@@ -56,7 +64,7 @@ def init_model(state: State):
     cost = {}
     for a in range(state.agents):
         cost[a] = model.new_int_var(0, state.time, f"cost_{a}")
-        model.add(cost[a] == sum([at_goal[t, a] for t in range(state.time)]))
+        model.add(cost[a] == sum([at_goal[t, a] for t in range(state.time)]) + 1)
 
     soc = model.new_int_var(0, state.time * state.agents, "soc")
     model.add(soc == sum(cost[a] for a in range(state.agents)))
