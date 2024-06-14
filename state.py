9bbe2f6725e99229ee169b44fd35d87f67ff12e8
FHanko <FlorianHanko@hotmail.com>
2024-06-14 14:31:47 +0200

    refactor conflict finding

--- state.py
+++ state.py
@@ -43,17 +43,23 @@ class State:
         else:
             return self.neighbor_random()
 
-    def neighbor_repair(self, neighborhood_size=10):
-        neighbor = State(self.agents, self.time, self.width, self.height, self.start, self.end, self.obstacles)
-        neighbor.paths = self.paths
-        # Select agents with conflicting paths as active agents.
+    def get_active_from_conflicts(self, max_size):
         for p1 in self.paths:
             for p2 in self.paths:
                 for t in range(self.time):
                     if (p1 != p2 and t < len(self.paths[p1]) and t < len(self.paths[p2])
                             and self.paths[p1][t] == self.paths[p2][t]):
-                        neighbor.active_agents.add(p1)
-                        neighbor.active_agents.add(p2)
+                        self.active_agents.add(p1)
+                        self.active_agents.add(p2)
+            if len(self.active_agents) >= max_size:
+                return
+
+    def neighbor_repair(self, neighborhood_size=10):
+        neighbor = State(self.agents, self.time, self.width, self.height, self.start, self.end, self.obstacles)
+        neighbor.paths = self.paths
+        # Select agents with conflicting paths as active agents.
+        neighbor.get_active_from_conflicts(neighborhood_size)
+
         # Set neighbor attributes.
         neighbor.agents = len(neighbor.active_agents)
         neighbor.obstacles = neighbor.get_obstacles()
